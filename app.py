99b4f37a14db05161b1f6310e358d66b798aa1bb
krishnakedia8-alt <krishnakedia8@gmail.com>
2025-11-16 00:39:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -208,5 +208,10 @@ def resolve(gid):
     return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+    try:
+        from app import init_db
+        init_db()
+    except:
+        pass
+    app.run(host='0.0.0.0', port=5000)
+
